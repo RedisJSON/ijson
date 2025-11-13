39813023e43963a99cc76fa11842ee748f1a0e7c
avivdavid23 <avivdavid2312@gmail.com>
2025-11-13 16:32:34 +0200

    remove print

--- fuzz/fuzz_targets/fuzz_json_de.rs
+++ fuzz/fuzz_targets/fuzz_json_de.rs
@@ -49,7 +49,6 @@ impl JsonValue {
 
 fuzz_target!(|value: JsonValue| {
     let json_string = value.to_json_string();
-    println!("json_string: {}", json_string);
     let mut deserializer = serde_json::Deserializer::from_str(&json_string);
     let _ = IValue::deserialize(&mut deserializer);
 });
